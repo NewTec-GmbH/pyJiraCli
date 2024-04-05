055a13aee612dc73d8f1e72a5616905fe88523bc
Timo Heitzmann <heitzmann_timo@web.de>
2024-04-05 09:17:38 +0200

    fix folder structure

--- coding/src/pyjira.py
+++
@@ -1,9 +0,0 @@
-from jira import JIRA
-
-jira = JIRA('https://jira.newtec.zz/secure/Dashboard.jspa', )
-
-issue = jira.issue('K11123P25-12')
-
-print(issue.fields.project.key)            # 'JRA'
-print(issue.fields.issuetype.name)         # 'New Feature'
-print(issue.fields.reporter.displayName)   # 'Mike Cannon-Brookes [Atlassian]'
