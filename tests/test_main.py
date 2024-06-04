695c558de7253d855c7029474b7fb995b5602932
Gabryel Reyes <gabryel.reyes@newtec.de>
2024-06-04 15:22:39 +0200

    Removed fancy lists

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,13 +3,12 @@ Tests for the pyJiraCli command line interface.
 """
 
 import subprocess
-from typing import List
 
 PYTHON_EXECUTABLE = "python"
 PYJIRACLI_EXECUTABLE = "src/pyJiraCli/__main__.py"
 
 
-def run_pyjiracli(arguments: List[str]) -> subprocess.CompletedProcess[bytes]:
+def run_pyjiracli(arguments) -> subprocess.CompletedProcess[bytes]:
     """
     Wrapper to run pyJiraCli command line.
 
