3ff3e04a9e3d104ca5dd62e2e5215dca98f9e15b
Gabryel Reyes <gabryel.reyes@newtec.de>
2024-06-12 13:06:03 +0200

    Import test. Changed examples so these can be used with the tests.

---
+++ tests/online/test_import.py
@@ -0,0 +1,90 @@
+"""
+Tests for the import command.
+Requires a Jira instance to be running.
+"""
+
+# BSD 3-Clause License
+#
+# Copyright (c) 2024, NewTec GmbH
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICU5LAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USEd
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+################################################################################
+# Imports
+################################################################################
+
+from pyJiraCli.ret import Ret
+from tests.conftest import Helpers
+
+################################################################################
+# Variables
+################################################################################
+
+################################################################################
+# Classes
+################################################################################
+
+################################################################################
+# Functions
+################################################################################
+
+
+def test_import(helpers: Helpers):
+    """ Test the import operation. """
+
+    # Make sure the profile exists by removing and adding it again.
+
+    # Remove existing profile. Ignore if failed.
+    helpers.remove_profile()
+
+    # Create the profile.
+    ret = helpers.create_profile()
+    assert Ret.CODE.RET_OK == ret.returncode
+
+    # Import a single issue.
+    ret = helpers.run_pyjiracli(
+        ["--profile", helpers.CI_PROFILE_NAME,
+         "import", "examples/import_issues/single_issue.json"])
+
+    assert Ret.CODE.RET_OK == ret.returncode
+
+    # Import multiple issues.
+    ret = helpers.run_pyjiracli(
+        ["--profile", helpers.CI_PROFILE_NAME,
+         "import", "examples/import_issues/multiple_issues.json"])
+
+    assert Ret.CODE.RET_OK == ret.returncode
+
+    # Import sub-issues.
+    ret = helpers.run_pyjiracli(
+        ["--profile", helpers.CI_PROFILE_NAME,
+         "import", "examples/import_issues/sub_issues.json"])
+
+    assert Ret.CODE.RET_OK == ret.returncode
+
+################################################################################
+# Main
+################################################################################
