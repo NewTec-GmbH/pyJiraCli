091fe9907a83bf5bbdbc589a77cfd1a99dccfcff
Gabryel Reyes <gabryel.reyes@newtec.de>
2024-06-14 10:08:00 +0200

    Create board with sprints and refactored setup script

--- tests/tools/setup_jira_server.py
+++ tests/tools/setup_jira_server.py
@@ -53,28 +53,41 @@ CI_JIRA_USER = "jira_user"
 CI_JIRA_USER_FULL_NAME = "Newly Created CI User"
 CI_JIRA_USER_PASSWORD = "jira"
 CI_JIRA_TEST_PROJECT = "TESTPROJ"
+CI_FILTER_NAME = "CI_FILTER"
+CI_FILTER_DESCRIPTION = "CI_FILTER_DESCRIPTION"
+CI_FILTER_JQL = "type = Bug and resolution is empty"
+CI_BOARD_NAME = "CI_BOARD"
+CI_SPRINT_NAME = "CI_SPRINT"
 
 ################################################################################
 # Classes
 ################################################################################
 
+################################################################################
+# Functions
+################################################################################
 
-def add_user_to_jira() -> None:
+
+def _add_user_to_jira(jira: JIRA) -> str:
     """Add a user to Jira Server for CI testing purposes."""
+    created_user_key = ""
 
     try:
         # Create user.
-        JIRA(
-            CI_JIRA_URL,
-            basic_auth=(CI_JIRA_ADMIN, CI_JIRA_ADMIN_PASSWORD),
-        ).add_user(
+        jira.add_user(
             username=CI_JIRA_USER,
             email="user@example.com",
             fullname=CI_JIRA_USER_FULL_NAME,
             password=CI_JIRA_USER_PASSWORD,
+            ignore_existing=True
         )
 
-        print("user", CI_JIRA_USER)
+        user_list = jira.search_users(user=CI_JIRA_USER)
+        created_user = user_list[0]
+
+        created_user_key = created_user.key
+        print("user", created_user.name)
+        print("user key", created_user_key)
 
     except Exception as e:  # pylint: disable=broad-except
         if "username already exists" not in str(e):
@@ -94,21 +107,23 @@ def add_user_to_jira() -> None:
         token = response.json().get("rawToken")
         print("token", token)
 
-        # Save token to GitHub environment.
-        # pylint: disable=W1514
-        with open(os.environ['GITHUB_ENV'], 'a') as fh:
-            print(f'{"CI_JIRA_USER_TOKEN"}={token}', file=fh)
+        try:
+
+            # Save token to GitHub environment.
+            # pylint: disable=W1514
+            with open(os.environ['GITHUB_ENV'], 'a') as fh:
+                print(f'{"CI_JIRA_USER_TOKEN"}={token}', file=fh)
+        except Exception:  # pylint: disable=broad-except
+            # GITHUB_ENV only exists in the CI.
+            pass
+
+    return created_user_key
 
 
-def create_project() -> None:
+def _create_project(jira: JIRA) -> None:
     """Create a project in Jira Server for CI testing purposes."""
 
     try:
-        jira = JIRA(
-            CI_JIRA_URL,
-            basic_auth=(CI_JIRA_ADMIN, CI_JIRA_ADMIN_PASSWORD),
-        )
-
         project = jira.create_project(CI_JIRA_TEST_PROJECT)
 
         if project is False:
@@ -123,14 +138,72 @@ def create_project() -> None:
             raise e
 
 
-def create_cert() -> None:
+def _create_cert() -> None:
     """Create a certificate for Jira Server for CI testing purposes."""
 
     with open("cert.pem", "w", encoding="UTF-8") as cert_file:
         cert_file.write("HELLO WORLD")
+        print("cert.pem created.")
+
+
+def _create_sprint(jira: JIRA, created_user_key: str) -> None:
+    """Create a sprint for Jira Server for CI testing purposes."""
+    jira_filter = jira.create_filter(
+        CI_FILTER_NAME, CI_FILTER_DESCRIPTION, CI_FILTER_JQL)
+
+    if jira_filter is None:
+        print("Failed to create filter.")
+        return
+
+    board = jira.create_board(CI_BOARD_NAME, jira_filter.id)
+
+    if board is None:
+        print("Failed to create board.")
+        return
+
+    sprint = jira.create_sprint(CI_SPRINT_NAME, board.id)
+
+    if sprint is None:
+        print("Failed to create sprint.")
+        return
+
+    print(f"Board {CI_BOARD_NAME} with sprint {jira_filter.id} created.")
+
+    # pylint: disable=missing-timeout
+    response = requests.post(CI_JIRA_URL + f"/rest/api/2/filter/{jira_filter.id}/permission",
+                             auth=(CI_JIRA_ADMIN, CI_JIRA_ADMIN_PASSWORD),
+                             json={
+                                 "type": "user",
+                                 "userKey": created_user_key,
+                                 "view": True,
+                                 "edit": True
+                             })
+
+    if response.status_code != 201:
+        print("Failed to create filter shares with response code",
+              response.status_code)
+        print(response.json())
+        print(response.request)
+    else:
+        print("Filter shares created.")
+
+
+def _setup_server() -> None:
+    """Setup Jira Server for CI testing purposes."""
+
+    jira = JIRA(
+        CI_JIRA_URL,
+        basic_auth=(CI_JIRA_ADMIN, CI_JIRA_ADMIN_PASSWORD),
+    )
+
+    created_user_key = _add_user_to_jira(jira)
+    _create_project(jira)
+    _create_cert()
+    _create_sprint(jira, created_user_key)
+
 
 ################################################################################
-# Functions
+# Main
 ################################################################################
 
 
@@ -149,9 +222,7 @@ if __name__ == "__main__":
             # pylint: disable=missing-timeout
             requests.get(CI_JIRA_URL + "rest/api/2/permissions")
             print("JIRA IS REACHABLE")
-            add_user_to_jira()
-            create_project()
-            create_cert()
+            _setup_server()
             break
 
         except (requests.exceptions.Timeout, requests.exceptions.ConnectionError) as ex:
@@ -162,8 +233,3 @@ if __name__ == "__main__":
             raise TimeoutError(
                 f"Jira server wasn't reachable within timeout {TIMEOUT_MINS}"
             )
-
-
-################################################################################
-# Main
-################################################################################
